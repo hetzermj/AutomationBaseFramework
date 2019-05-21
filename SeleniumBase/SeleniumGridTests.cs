1d58eb9552cefc59e90d233db42b57e5cf8d9b04
michael.hetzer <michael.hetzer@MHETZER-PC>
2019-05-21 16:06:23 -0400

    Added selenium grid sample tests

---
+++ SeleniumBase/SeleniumGridTests.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Threading;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Remote;
+using NUnit.Framework;
+
+namespace SeleniumBase
+{
+	[TestFixture]	
+	public class SeleniumGridTests
+	{
+		ChromeOptions Options = null;
+
+		[SetUp]
+		public void Setup()
+		{
+			ChromeOptions Options = new ChromeOptions();
+			Options.AddArgument("--no-sandbox"); // Bypass OS security model, MUST BE THE VERY FIRST OPTION			
+			Options.AddArgument("start-maximized"); // open Browser in maximized mode
+			Options.AddArgument("disable-infobars"); // disabling infobars
+			Options.AddArgument("--disable-extensions"); // disabling extensions
+			Options.AddArgument("--disable-gpu"); // applicable to windows os only
+			Options.AddArgument("--disable-dev-shm-usage"); // overcome limited resource problems
+			this.Options = Options;
+		}
+
+
+
+		[Test, Parallelizable]
+		public void TestMethod1()
+		{
+			RemoteWebDriver driver = new RemoteWebDriver(new Uri("http://10.20.1.42:4444/wd/hub"), Options);			
+			driver.Navigate().GoToUrl("http://www.google.com");
+			Thread.Sleep(1000);
+			driver.Quit();
+		}
+
+		[Test, Parallelizable]
+		public void TestMethod2()
+		{
+			RemoteWebDriver driver = new RemoteWebDriver(new Uri("http://10.20.1.42:4444/wd/hub"), Options);
+			driver.Navigate().GoToUrl("http://www.google.com");
+			Thread.Sleep(1000);
+			driver.Quit();
+		}
+
+		[Test, Parallelizable]
+		public void TestMethod3()
+		{
+			RemoteWebDriver driver = new RemoteWebDriver(new Uri("http://10.20.1.42:4444/wd/hub"), Options);
+			driver.Navigate().GoToUrl("http://www.google.com");
+			Thread.Sleep(1000);
+			driver.Quit();
+		}
+
+		[Test, Parallelizable]
+		public void TestMethod4()
+		{
+			RemoteWebDriver driver = new RemoteWebDriver(new Uri("http://10.20.1.42:4444/wd/hub"), Options);
+			driver.Navigate().GoToUrl("http://www.google.com");
+			Thread.Sleep(1000);
+			driver.Quit();
+		}
+
+	}
+}
---
+++ UIAutomationClientBase/WhiteWinformsAppTest.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UIAutomationClientBase.Elements;
+using NUnit.Framework;
+using System.Threading;
+
+namespace UIAutomationClientBase
+{
+	[TestFixture]
+	public class WhiteWinformsAppTest
+	{
+
+		[Test]
+		public void BeginnerTest()
+		{
+			Application app = new Application(DesktopApp.WhiteWinFormsTestApp);
+
+			app.LaunchApplication();
+
+			Thread.Sleep(5000);
+
+			app.KillApplication();
+
+		}
+
+
+
+	}
+}
