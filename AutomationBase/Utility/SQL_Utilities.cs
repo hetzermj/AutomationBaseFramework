30d589232fbb7ececac8e3d5a3099dac6acb2ee1
michael.hetzer <michael.hetzer@MHETZER-PC>
2019-06-03 18:14:48 -0400

    Added example to work with for the SQLUtilities Class

--- AutomationBase/Utility/SQL_Utilities.cs
+++ AutomationBase/Utility/SQL_Utilities.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -19,5 +21,39 @@ namespace AutomationBase.Utility
 
             return obj.ToString();
         }
+
+
+		public static void DoSomethingWithTableData()
+		{
+			SqlConnection sqlConn = new SqlConnection(SQL_Utilities.CONNECTION_STRING);
+			SqlCommand cmd = new SqlCommand();
+			SqlDataReader data;
+
+			string select = "WhatYouWantToSelect";
+			string tableName = "";
+			string columnName = "";
+			string match = "";
+
+			//	Getting the Seed Loan ID from the TestInformation Table
+			cmd.CommandText =
+				$"SELECT {select} " +
+				$"FROM {tableName} " +
+				$"WHERE {columnName}='{match}';"
+				;
+
+			cmd.CommandType = CommandType.Text;
+			cmd.Connection = sqlConn;
+
+			sqlConn.Open();
+
+			data = cmd.ExecuteReader();
+			data.Read();
+
+			string dummyData = data["ColumnName"].ToString();
+
+			sqlConn.Close();
+
+		}
+
 	}
 }
